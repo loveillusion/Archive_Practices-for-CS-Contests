6fb1d1b73fce676c03d932d41fc332164af1e9b8
LoveiLLUSiON <74571080+loveillusion@users.noreply.github.com>
2022-02-09 21:53:00 -0500

    Delete Written Using Visual Studio Code/PJ7 Binary Search Algorithm & Greedy directory

--- Written Using Visual Studio Code/PJ7 Binary Search Algorithm & Greedy/what is going on.cpp
+++
@@ -1,9 +0,0 @@
-#include <cstdio>
-#include <algorithm>
-
-using namespace std;
-
-int main() {
-    int a[5] = {0, 2, 4, 6, 8};
-    printf("%d %d %d\n", lower_bound(a, a + 5, 2) - a, upper_bound(a, a + 5, 2) - a, binary_search(a, a + 5, 2));
-}
